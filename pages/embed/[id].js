787721b475919919e469a1503b06f6b02e6ac2cc
Arriv9l <imaginistx12@gmail.com>
2021-02-02 11:43:43 +0800

    change [id].js

--- pages/embed/[id].js
+++ pages/embed/[id].js
@@ -5,7 +5,7 @@ import EmbedPage from './index'
 
 import api from '../../lib/api'
 
-export async function getServerSideProps({ req, res, query }) {
+export async function getStaticProps({ req, res, query }) {
   const { id: path, filename } = query
 
   const parameter = path.length >= 19 && path.indexOf('.') < 0 ? path : null
